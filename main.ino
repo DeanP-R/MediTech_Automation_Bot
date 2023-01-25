c2c11b9ae2b0b80e41e90794f7da27b574b5975a
ReeceAlexander <reecealex230503@gmail.com>
2023-01-25 08:55:52 +0000

    Added a list of potential sensors

--- main.ino
+++ main.ino
@@ -3,5 +3,10 @@ void setup() {
 }
 
 void loop() {
-    
+
 }
+
+/*
+Realistically we'll probs have 2 motors, 4-6 ultrasonic sensors, 1 line following module, 1 colour module, 1 latch, 1 keypad, 1 rfid module, 1-2 lcd screens, weight sensor 
+or another ultrasonic sensor to tell the robot when to go. Can't think of much else atm but ill ask at the meeting and see what they're saying 
+*/
