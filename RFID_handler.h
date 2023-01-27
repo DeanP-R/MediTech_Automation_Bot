88f1b60f9edcb85c874b35ae8d00027604a43df8
DeanP-R <Deanrowlett00@gmail.com>
2023-01-27 13:09:20 +0000

    header file declarations

--- RFID_handler.h
+++ RFID_handler.h
@@ -1,60 +1,24 @@
+/*
+    Declerations for RFID_Handler
+*/
 #ifndef RFID_HANDLER_H
 #define RFID_HANDLER_H
 
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Servo.h>
 
 #define SS_PIN 10
 #define RST_PIN 9
+#define SERVO_PIN 3
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);
+extern Servo myservo;
+extern MFRC522 mfrc522;
+extern bool authorised;
 
-void setupRFID();
-bool checkAuthorized();
+void rfid_setup();
+bool check_user();
+//void RFID_handler();
+
 
 #endif
-
-void setupRFID() {
-  Serial.begin(9600);
-  SPI.begin();
-  mfrc522.PCD_Init();
-  myservo.attach(SERVO_PIN);
-  myservo.write(0);
-  Serial.println("Please scan your card");
-  Serial.println();
-}
-
-bool checkAuthorized() {
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return false;
-  }
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return false;
-  }
-
-  String content = "";
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  content.toUpperCase();
-
-  if (content.substring(1) == "F4 11 4F 60") {
-    if(!authorised){
-      Serial.println("Authorized access");
-      myservo.write(180);
-      authorised = true;
-    }
-    else{
-      Serial.println("Storage Locked");
-      myservo.write(0);
-      authorised = false;
-    }
-    delay(3000);
-  } else {
-    Serial.println("Access denied");
-    myservo.write(0);
-    authorised = false;
-    delay(3000);
-  }
-}
