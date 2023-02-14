1a22bc78bf497b0ecc64fa2448462bb2e1659bb9
DeanP-R <Deanrowlett00@gmail.com>
2023-02-14 14:17:43 +0000

    updated version history

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,13 @@
 /****************************************************************************************
   File Name       : MediTech Automation Control                                         *
-  Version         : 0.1.0                                                               *
+  Version         : 0.1.5                                                               *
   Description     : Main code for B39VS - System Project                                *
   Author          : Dean Rowlett & Reece Alexander                                      *
   Target          : Arduino Uno Revision 3                                              *
   Compiler        : avr-g++                                                             *
   IDE             : Arduino 2.0.3                                                       *
   Programmer      : Arduino ISP (In-System Programmer)                                  *
-  Last Updated    : 27th January 2023                                                   *
+  Last Updated    : 13th February 2023                                                  *
 ****************************************************************************************/
 #include "RFID_handler.h"
 #include "track_movement.h"
