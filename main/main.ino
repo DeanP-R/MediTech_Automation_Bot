d90e30f235337fcc1f293e73423d3a9fa5cbca4a
DeanP-R <Deanrowlett00@gmail.com>
2023-02-14 14:16:46 +0000

    updated for week 4 progress

--- main/main.ino
+++ main/main.ino
@@ -10,24 +10,12 @@
   Last Updated    : 27th January 2023                                                   *
 ****************************************************************************************/
 #include "RFID_handler.h"
-
-Servo myservo;
-bool authorised = false;
-MFRC522 mfrc522(SS_PIN, RST_PIN);
-
+#include "track_movement.h"
 void setup() {
-  Serial.begin(9600);
-  rfid_setup();
+  setup_Motor_Control();
 
 }
 
 void loop() {
-  check_user();
-
+  forward();
 }
-
-
-/*
-Realistically we'll probs have 2 motors, 4-6 ultrasonic sensors, 1 line following module, 1 colour module, 1 latch, 1 keypad, 1 rfid module, 1-2 lcd screens, weight sensor 
-or another ultrasonic sensor to tell the robot when to go. Can't think of much else atm but ill ask at the meeting and see what they're saying 
-*/
