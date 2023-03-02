64625e9733aa90677e51c4a9cf7f6defab5182d5
DeanP-R <75754203+DeanP-R@users.noreply.github.com>
2023-03-02 14:37:42 +0000

    Update track_movement.h

--- main/track_movement.h
+++ main/track_movement.h
@@ -16,3 +16,22 @@ void forward();
 void reverse();
 void turn_Right();
 void turn_Left();
+
+/*
+
+//porportional line following
+
+#define MOT_A1_PIN 5
+#define MOT_A2_PIN 6
+#define EN1_PIN 3
+
+#define MOT_B1_PIN 9
+#define MOT_B2_PIN 10
+#define EN2_PIN 11
+
+void setupMotors();
+void moveForward();
+void turnRight();
+void turnLeft();
+void scan();
+void updateDirection();
