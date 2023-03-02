1676c84727f928a32a97d6bcdbfbe40d8eee2369
ReeceAlexander <reecealex230503@gmail.com>
2023-03-02 13:25:21 +0000

    changed main

--- main/main.ino
+++ main/main.ino
@@ -7,18 +7,47 @@
   Compiler        : avr-g++                                                             *
   IDE             : Arduino 2.0.3                                                       *
   Programmer      : Arduino ISP (In-System Programmer)                                  *
-  Last Updated    : 22nd February 2023                                                  *
+  Last Updated    : 23nd February 2023                                                  *
 ****************************************************************************************/
-#include "RFID_handler.h"
-#include "track_movement.h"
 #include "proportional_line_following.h"
-void setup() {
-  setupMotors();
+#include "colour_sensing.h"
 
+// Create an array for colour sensor values. - Reece
+int RGB[3];
+
+String state = "inTransit";
+
+void setup() {
+  // Serial.begin(9600);
+  setupMotors();
+  setupColourSensor(); 
 }
 
 void loop() {
-  moveForward(200);
+  readColourSensor(RGB); 
+
+  if (RGB[0] > 140) {
+    stop();
+    state = "stopped";
+
+  } else if (RGB[2] > 100) {
+    turnLeft(255);
+    delay(1000);
+    moveForward(200);
+
+  } else {
+    moveForward(200);
+    state = "inTransit";
+  }
+  // Serial.print("R: ");
+  // Serial.print(RGB[0]);
+  // Serial.print(" G: ");
+  // Serial.print(RGB[1]);
+  // Serial.print(" B: ");
+  // Serial.print(RGB[2]);
+  // Serial.print(" ");
+  // Serial.println(reading);
+  
   scan();
-  updateDirection(2);
+  updateDirection(2, state);
 }
