32fc1bdfdb30dfda00576e2a4f28064d73118c9e
DeanP-R <75754203+DeanP-R@users.noreply.github.com>
2023-03-15 23:11:50 +0000

    Updated pin connections

--- main/proportional_line_following.h
+++ main/proportional_line_following.h
@@ -3,13 +3,13 @@
 #include <Arduino.h>
 
 // Define the motor pins. - Reece
-#define MOT_A1_PIN 5
-#define MOT_A2_PIN 6
-#define EN1_PIN 3
+#define MOT_A1_PIN 25
+#define MOT_A2_PIN 26
+#define EN1_PIN 10
 
-#define MOT_B1_PIN 9
-#define MOT_B2_PIN 10
-#define EN2_PIN 11
+#define MOT_B1_PIN 27
+#define MOT_B2_PIN 28
+#define EN2_PIN 9
 
 void setupMotors();
 void moveForward(int pwmSpeed);
