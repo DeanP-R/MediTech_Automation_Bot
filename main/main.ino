3d2e4a8d2cb8367dab173d7fdeeae42148e79a38
DeanP-R <Deanrowlett00@gmail.com>
2023-03-17 23:03:44 +0000

    laser method formatting

--- main/main.ino
+++ main/main.ino
@@ -36,6 +36,7 @@ void setup() {
   //lockSetup();
   // lcd1.init();
   // lcd1.backlight(); 
+  setupLasers();
 }
 
 void loop() {
--- main/object_avoidance.cpp
+++ main/object_avoidance.cpp
@@ -26,92 +26,55 @@ VL53L0X_RangingMeasurementData_t leftValue;
 VL53L0X_RangingMeasurementData_t centerValue;
 VL53L0X_RangingMeasurementData_t rightValue;
 
+
+
+
 // Method to initialize the laser sensors
 void setupLasers() {
-   // Initialize the left sensor ------------------------------------
-  digitalWrite(SHT_LEFT, HIGH);
-  delay(10);
-
+  // ... other setup code ...
+  
+  // Initialize the laser sensors
   TCA9548A(0);
-  if (!leftLaser.begin(LEFT_ADDRESS)) {
-    Serial.println("Setup Error: Left Laser");
-    while (1);
-  }
-  Serial.println("Left Laser Setup Success");
-  delay(1000);
-
-  // Activate the center sensor --------------------------------------
-  digitalWrite(SHT_CENTER, HIGH);
-  delay(10);
-
+  leftLaser.begin(LEFT_ADDRESS);
   TCA9548A(1);
-  // Initialize the center sensor
-  if (!centerLaser.begin(CENTER_ADDRESS)) {
-    Serial.println("Setup Error: Center Laser");
-    while (1);
-  }
-  Serial.println("Center Laser Setup Success");
-  delay(1000);
-
-  // Activate the right sensor ------------------------------
-  digitalWrite(SHT_RIGHT, HIGH);
-  delay(10);
-
-  // Initialize the right sensor
+  centerLaser.begin(CENTER_ADDRESS);
   TCA9548A(2);
-  if (!rightLaser.begin(RIGHT_ADDRESS)) {
-    Serial.println("Setup Error: Right Laser");
-    while (1);
-  }
-  Serial.println("Right Laser Setup Success");
-  delay(1000);
+  rightLaser.begin(RIGHT_ADDRESS);
 }
-
 // Method to read the laser sensors and save the data into an array
+
 void readLaserSensors() {
   // Read the left sensor and save the data into leftValue
-  //Serial.println("Selecting channel 0");
   TCA9548A(0);
-  //Serial.println("Selected channel 0");
-  if (leftLaser.begin(LEFT_ADDRESS)) {
-    leftLaser.rangingTest(&leftValue, false);
-    LCR[0] = leftValue.RangeMilliMeter;
-    Serial.print("Left Value: ");
-    Serial.print(LCR[0]);
-  }
+  leftLaser.rangingTest(&leftValue, false);
+  LCR[0] = leftValue.RangeMilliMeter;
+  Serial.print(" Left Reading: ");
+  Serial.print(LCR[0]);
 
   // Read the center sensor and save the data into centerValue
-  //Serial.println("Selecting channel 1");
   TCA9548A(1);
-  //Serial.println("Selected channel 1");
-  if (centerLaser.begin(RIGHT_ADDRESS)) {
-    centerLaser.rangingTest(&centerValue, false);
-    LCR[1] = centerValue.RangeMilliMeter;
-    Serial.print(" | Center value: ");
-    Serial.print(LCR[1]);
-  }
-  // Read the right sensor and save the data into rightValue
-  //Serial.println("Selecting channel 2");
-  TCA9548A(2);
-  //Serial.println("Selected channel 2");
-  if (rightLaser.begin(RIGHT_ADDRESS)) {
-    rightLaser.rangingTest(&rightValue, false);
-    LCR[2] = rightValue.RangeMilliMeter;
-    Serial.print(" | Right Value:" );
-    Serial.println(LCR[2]);
-  }
-  // Convert the range data into millimeters and save each sensor's distance value into the corresponding index of the passed array
+  centerLaser.rangingTest(&centerValue, false);
+  LCR[1] = centerValue.RangeMilliMeter;
+  Serial.print(" | Center Reading: ");
+  Serial.print(LCR[1]);
 
-  //readLaserSensors(LCR);
-  // Serial.print("Lasers: ");
-  // Serial.print("L: ");
-  // Serial.print(LCR[0]);
-  // Serial.print(" C: ");
-  // Serial.print(LCR[1]);
-  // Serial.print(" R: ");
-  // Serial.print(LCR[2]);
-  // Serial.println(" ");
+  // Read the right sensor and save the data into rightValue
+  TCA9548A(2);
+  rightLaser.rangingTest(&rightValue, false);
+  LCR[2] = rightValue.RangeMilliMeter;
+  Serial.print(" | Right Reading: ");
+  Serial.println(LCR[2]);
+
+  // Print the sensor values, if necessary
+  //Serial.print("Left Value: ");
+  //Serial.print(LCR[0]);
+  //Serial.print(" | Center value: ");
+  //Serial.print(LCR[1]);
+  //Serial.print(" | Right Value:" );
+  //Serial.println(LCR[2]);
 }
+
+//------------------------
 
 // Method to swerve right to avoid an object
 void swerveRight() {
