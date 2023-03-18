fd39bede60cfbac941aea0e14c5cf63b53a7959d
DeanP-R <Deanrowlett00@gmail.com>
2023-03-18 12:05:52 +0000

    MUX implementation
    updated colour sensing for integration with I2C mutliplexer, updated documentation

--- main/colour_sensing.cpp
+++ main/colour_sensing.cpp
@@ -1,18 +1,25 @@
-// Include the h file, 
+// Include the colour sensing header file
 #include "colour_sensing.h"
 
-// Create a colour sensor object. - Example colorview code
+// Create an object of the Adafruit_TCS34725 class for the colour sensor
 Adafruit_TCS34725 tcs = Adafruit_TCS34725();
+
+// Create an array to store the RGB values read from the colour sensor
+int RGB[3];
 
 /*
  * This method starts the colour sensor,
- * Assigns and initialises the colour sensor pins, 
- * Then creates a gamma table to convert the RGB data into observable human colours. - Example colorview code
+ * Assigns and initializes the pins used for the colour sensor,
+ * and sets up the pins used to control an LED. 
  */
 void setupColourSensor() {
+  // Configure the TCA9548A multiplexer to use channel 4
+  TCA9548A(3);
+  // Initialize the colour sensor with the I2C address 0x29
   tcs.begin(0x29);
-  
-  // use these three pins to drive an LED
+
+  // Use the following pins to drive an LED
+  // On ESP32 boards, use the LEDC interface to control the pins
 #if defined(ARDUINO_ARCH_ESP32)
   ledcAttachPin(redpin, 1);
   ledcSetup(1, 12000, 8);
@@ -20,6 +27,7 @@ void setupColourSensor() {
   ledcSetup(2, 12000, 8);
   ledcAttachPin(bluepin, 3);
   ledcSetup(3, 12000, 8);
+  // On other boards, set the pin modes to OUTPUT
 #else
   pinMode(redpin, OUTPUT);
   pinMode(greenpin, OUTPUT);
@@ -28,19 +36,24 @@ void setupColourSensor() {
 }
 
 /*
- * This method reads the colour sensor, 
- * Then populates a provided array with RGB values for the rest of the program to use. - Example colorview code
+ * This method reads the RGB values from the colour sensor,
+ * and stores them in the RGB array.
  */
-void readColourSensor(int* RGB) {
+void readColourSensor() {
   float red, green, blue;
-
-  tcs.setInterrupt(false);  // turn on LED
-
+  // Select channel 4 on the TCA9548A multiplexer
+  TCA9548A(3);
+  // Turn on the LED and disable the colour sensor interrupt
+  tcs.setInterrupt(false);
+  // Read the RGB values from the colour sensor
   tcs.getRGB(&red, &green, &blue);
-
-  tcs.setInterrupt(true);  // turn off LED
-
+  // Turn off the LED and enable the colour sensor interrupt
+  tcs.setInterrupt(true);
+  // Convert the RGB values to integers and store them in the RGB array
   RGB[0] = int(red);
   RGB[1] = int(green);
   RGB[2] = int(blue); 
+
+  // Print the RGB values to the serial port
+  Serial.print("Red Value: " + String(RGB[0]) + " Green Value: " + String(RGB[1]) + " Blue Value: " + String(RGB[2]) + " ---|--- ");
 }
--- main/colour_sensing.h
+++ main/colour_sensing.h
@@ -4,6 +4,7 @@
 #include <Wire.h>
 #include <SPI.h>
 #include "Adafruit_TCS34725.h"
+#include "multiplexer.h"
 
 // Define colour sensor output pins. - Example colorview code
 #define redpin 3
@@ -11,4 +12,4 @@
 #define bluepin 6
 
 void setupColourSensor();
-void readColourSensor(int* RGB);
+void readColourSensor();
