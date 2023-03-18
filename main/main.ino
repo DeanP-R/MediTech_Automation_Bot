c3684d3a4e41c9a0d613dddf5892506007747d60
DeanP-R <Deanrowlett00@gmail.com>
2023-03-18 12:06:26 +0000

    updated for colour sensing testing

--- main/main.ino
+++ main/main.ino
@@ -16,8 +16,7 @@
 #include "multiplexer.h"
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-// Create an array for colour sensor values. - Reece
-int RGB[3];
+
 int bus = 0;  // default channel on the multiplexer
 
 LiquidCrystal_I2C lcd1(0x27, 20, 4);
@@ -26,17 +25,12 @@ String state = "inTransit";
 
 void setup() {
   Serial.begin(9600);
-  //setupLasers();
-  //Serial1.begin(9600);
-  //setupMotors(); 
-  // setupLaserTest(laser, 38, 1);
-  // setupLaserTest(laser, 39, 2);
-  // setupLaserTest(laser, 40, 3);
-  //setupColourSensor();
-  //lockSetup();
-  // lcd1.init();
-  // lcd1.backlight(); 
   setupLasers();
+  setupMotors();
+  setupColourSensor();
+  //lockSetup();
+  //lcd1.init();
+  //lcd1.backlight(); 
 }
 
 void loop() {
@@ -45,49 +39,10 @@ void loop() {
   // scan();
   // updateDirection(2, state);
   //lock_op();
-  // readColourSensor(RGB); 
-  // Serial.print("Colour Sensor: ");
-  // Serial.print("R: ");
-  // Serial.print(RGB[0]);
-  // Serial.print(" G: ");
-  // Serial.print(RGB[1]);
-  // Serial.print(" B: ");
-  // Serial.print(RGB[2]);
-  // Serial.print(", ");
-  //Serial.println("enter loop");
   readLaserSensors();
-  // while(Serial1.available()>0){
-  //   //char uno_data = Serial1.read();
-  //   Serial.println(Serial1.read());
-
-  // if (RGB[0] > 140) {
-  //   stop();
-  //   state = "stopped";
-
-  // } else if (RGB[2] > 100) {
-  //   turnLeft(255); 
-  //   delay(1250);
-  //   moveForward(200);
-  // } else if (LCR[1] <= 150) {
-  //   if (LCR[0] < LCR[2] || LCR[0] <= 150) {
-  //     swerveRight();
-  //   } else if (LCR[2] < LCR[0] || LCR[2] <= 150) {
-  //     swerveLeft();
-  //   }
-
-  // } else {
-  // state = "inTransit";
-  // }
-    // select the desired channel on the multiplexer
+  readColourSensor();
+  // select the desired channel on the multiplexer
   // TCA9548A(channel);
-
-  // // print to the LCD screen
-  // lcd.setCursor(0, 0);  // set the cursor to the first position on the first line
-  // lcd.print("bAlLz!");  // print the message
-
-  // delay(1000);  // wait for a second before updating the display
-  // lcd1.setCursor(0,0);
-  // lcd1.print("BAllz"); 
 }
 
 /*
