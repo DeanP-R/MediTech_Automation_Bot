86606b5f342f6e2da3977ddfb70d59beac27529d
DeanP-R <75754203+DeanP-R@users.noreply.github.com>
2023-03-22 11:59:35 +0000

    added wire colouring

--- main/proportional_line_following.h
+++ main/proportional_line_following.h
@@ -3,13 +3,13 @@
 #include <Arduino.h>
 
 // Define the motor pins. - Reece
-#define MOT_A1_PIN 25
-#define MOT_A2_PIN 26
-#define EN1_PIN 10
+#define MOT_A1_PIN 25 //blue
+#define MOT_A2_PIN 26 //green 
+#define EN1_PIN 10    //green
 
-#define MOT_B1_PIN 27
-#define MOT_B2_PIN 28
-#define EN2_PIN 9
+#define MOT_B1_PIN 27 //purple
+#define MOT_B2_PIN 28 //yellow
+#define EN2_PIN 9     //blue
 
 void setupMotors();
 void moveForward(int pwmSpeed);
