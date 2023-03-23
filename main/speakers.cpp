1cb4e7ea87bcef0db6f82288111f20289f64d007
DeanP-R <75754203+DeanP-R@users.noreply.github.com>
2023-03-23 17:18:31 +0000

    updated speaker values

--- main/speakers.cpp
+++ main/speakers.cpp
@@ -4,6 +4,8 @@
 SoftwareSerial softwareSerial(PIN_MP3_RX, PIN_MP3_TX);
 DFRobotDFPlayerMini player;
 
+int music_wait = 5000;
+
 void speakerSetup() {
   // Init USB serial port for debugging
   Serial.begin(9600);
@@ -22,6 +24,54 @@ void speakerSetup() {
 }
 
 void runSpeaker() {
-  player.playMp3Folder(2);
-  delay(2000);
+  // player.playMp3Folder(1);  //Moving
+  // delay(music_wait);
+  // player.playMp3Folder(2);  //Obstacle detected
+  // delay(music_wait);
+  //  player.playMp3Folder(3);  //Please clear path
+  //  delay(music_wait);
+  //  player.playMp3Folder(4);  //Please scan RFID tag
+  //  delay(music_wait);
+  //  player.playMp3Folder(5);  //Please enter your PIN
+  //  delay(music_wait);
+  //  player.playMp3Folder(6);  //Unlocking
+  //  delay(music_wait);
+  //  player.playMp3Folder(7);  //Locking
+  //  delay(music_wait);
+  //  player.playMp3Folder(8);  //Delivery in progress
+  //  delay(music_wait);
+  //  player.playMp3Folder(9);  //Delivery complete
+  //  delay(music_wait);
+  //  player.playMp3Folder(10); //On route to
+  //  delay(music_wait);
+  //  player.playMp3Folder(11); //Arrived at
+  //  delay(music_wait);
+  //  player.playMp3Folder(12); //A & E
+  //  delay(music_wait);
+  //  player.playMp3Folder(13); //Pharmacy
+  //  delay(music_wait);
+  //  player.playMp3Folder(14); //Maternity
+  //  delay(music_wait);
+  //  player.playMp3Folder(15); //Radiology
+  //  delay(music_wait);
+  //  player.playMp3Folder(16); //Outpatients
+  //  delay(music_wait);
+  //  player.playMp3Folder(17); //Home
+  //  delay(music_wait);
+  //  player.playMp3Folder(18); //Re-calculating route
+  //  delay(music_wait);
+  //  player.playMp3Folder(19); //Correct tag deteced
+  //  delay(music_wait);
+  //  player.playMp3Folder(20); //Correct pin detected
+  //  delay(music_wait);
+  //  player.playMp3Folder(21); //Incorrect tag detected
+  //  delay(music_wait);
+  //  player.playMp3Folder(22); //Incorrect pin detected
+  //  delay(music_wait);
+  //  player.playMp3Folder(23); //Access granted
+  //  delay(music_wait);
+  //  player.playMp3Folder(24); //Access denied
+  //  delay(music_wait);
+  //  player.playMp3Folder(25); //Battery fully charged
+  //  delay(music_wait);
 }
--- main/speakers.h
+++ main/speakers.h
@@ -9,6 +9,5 @@
 static const uint8_t PIN_MP3_TX = 2;  // Connects to module's RX
 static const uint8_t PIN_MP3_RX = 3;  // Connects to module's TX
 
-
 void speakerSetup();
 void runSpeaker();
